80fcd270f876955b0d54b6926480da3db7263d1f
Okan <nayko62@gmail.com>
2024-05-18 12:29:48 +0300

    up: better status button

--- src/components/StatusButton.tsx
+++ src/components/StatusButton.tsx
@@ -13,13 +13,13 @@ function ButtonStatus({
 }) {
   return currentStatus === status ? (
     <button
-      className='bg-neutral-100 text-neutral-900 hover:bg-neutral-200 rounded-lg duration-150 px-3 py-1.5 dark:bg-neutral-950 dark:text-neutral-100 dark:hover:bg-neutral-950/40 first-letter:uppercase'
+      className='bg-neutral-100 text-neutral-900 rounded-lg duration-150 px-3 py-1.5 dark:bg-neutral-950 dark:text-neutral-100'
       onClick={() => setStatus(status)}>
       <Icon />
     </button>
   ) : (
     <button
-      className='text-neutral-500 hover:bg-neutral-200 rounded-lg duration-150 px-3 py-1.5 dark:text-neutral-600 dark:hover:bg-neutral-950/40 first-letter:uppercase'
+      className='text-neutral-500 hover:bg-neutral-200 rounded-lg duration-150 px-3 py-1.5 dark:text-neutral-600 dark:hover:bg-neutral-950/40'
       onClick={() => setStatus(status)}>
       <Icon />
     </button>
