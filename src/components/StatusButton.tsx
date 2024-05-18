553adf0ff44c51e836e73dec11e324da3e02cff2
Okan <nayko62@gmail.com>
2024-05-18 18:37:04 +0300

    up: smoother border radius

--- src/components/StatusButton.tsx
+++ src/components/StatusButton.tsx
@@ -13,13 +13,13 @@ function ButtonStatus({
 }) {
   return currentStatus === status ? (
     <button
-      className='bg-neutral-200 text-neutral-900 rounded-none last:rounded-r-lg first:rounded-l-lg duration-150 px-3 py-1.5 dark:bg-neutral-700 dark:text-neutral-100'
+      className='bg-neutral-200 text-neutral-900 rounded-none last:rounded-r-xl first:rounded-l-xl duration-150 px-3 py-1.5 dark:bg-neutral-700 dark:text-neutral-100'
       onClick={() => setStatus(status)}>
       <Icon />
     </button>
   ) : (
     <button
-      className='text-neutral-500 hover:bg-neutral-300 rounded-none last:rounded-r-lg first:rounded-l-lg duration-150 px-3 py-1.5 dark:text-neutral-600 dark:hover:bg-neutral-700/60'
+      className='text-neutral-500 hover:bg-neutral-300 rounded-none last:rounded-r-xl first:rounded-l-xl duration-150 px-3 py-1.5 dark:text-neutral-600 dark:hover:bg-neutral-700/60'
       onClick={() => setStatus(status)}>
       <Icon />
     </button>
